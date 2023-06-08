5e7a6600d185ea67797b2f608e8fe94fada73093
eshan36 <125075911+eshan36@users.noreply.github.com>
2023-06-08 09:08:43 +0600

    Update velocity contour.c

--- velocity contour.c
+++ velocity contour.c
@@ -1,19 +1,21 @@
- event figure_velocityyy (t = 0.1; t <= MAXTIME; t += 0.1)
+#include "iso3D.h"
+event figure_velocityyy (t = 0.1; t <= MAXTIME; t += 0.1)
 {
   view (camera = "top", fov = 3, quat = {0,0,0,1}, tx = 0.0, ty = -0.03, bg = {1,1,1}, width = 1080, height = 1920, samples = 4);
-
+\\setting the position of camera
   clear();
 
   scalar omegay[],mag_u[];
   double yb = 0., sb = 0.;
-
+	 
+  \\position of center of bubble in y axis
    foreach(reduction(+:yb) 
 	  reduction(+:sb)) {
     double dv = (1. - f[])*dv();
     yb += y*dv;
     sb += dv;
   }
-  
+ 
   foreach()
     mag_u[] = sqrt(sq(u.x[])+sq(u.y[])+sq(u.z[]));
   
